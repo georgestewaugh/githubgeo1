9e3b182e3416152c9981f91ebd91656fd2736310
georgestewaugh <stewaughgeorge@gmail.com>
2025-01-28 15:43:30 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var a = 110;
+var b = 300;  
+
+function added(a,b)
+{
+console.log(a+b)
+}
+added(110,300)
+//////////////-----------...spread using array-------------//////////////////
+const toarray=(...args)=>{
+    return(args)
+}
+console.log(toarray(1,2,34,4,56,7,7));
